47c20aa2f2fe66f864f8d887a339b21e003d2890
Marc Pomar <marc@faable.com>
2019-04-17 13:21:25 +0200

    dd

---
+++ calculos/ejercicios.py
@@ -0,0 +1,6 @@
+import random
+
+
+def calculaEjercicio():
+    nota = random.random()
+    print("Tu nota en este ejercicio es {:.2f}".format(nota))
---
+++ calculos/notas.py
@@ -0,0 +1,10 @@
+import random
+
+
+def calculaNota():
+    nota = random.random()
+    print("Tu nota es {:.2f}".format(nota))
+
+
+if __name__ == "__main__":
+    calculaNota()
